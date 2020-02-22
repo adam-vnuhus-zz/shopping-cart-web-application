2fbff89937509247bef1f050a6865414074d9d9f
anhtdam <tuananh1108.97@gmail.com>
2020-02-22 08:54:30 +0700

    update createUser

--- src/main/java/com/example/usermanagementrestapi/controller/UserController.java
+++ src/main/java/com/example/usermanagementrestapi/controller/UserController.java
@@ -2,12 +2,17 @@ package com.example.usermanagementrestapi.controller;
 
 import com.example.usermanagementrestapi.entity.User;
 import com.example.usermanagementrestapi.model.dto.UserDto;
+import com.example.usermanagementrestapi.model.request.CreateUserReq;
 import com.example.usermanagementrestapi.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/users")
@@ -17,21 +22,39 @@ public class UserController {
     @Autowired
     public UserService userService;
 
+    @ApiOperation(value = "Lấy danh sách ", response = UserDto.class, responseContainer = "List")
+    @ApiResponses({
+            @ApiResponse(code=500, message = "")
+    })
     @GetMapping("")
     public ResponseEntity<?> getListUser() {
         List<UserDto> result = userService.getListUser();
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
+    @ApiOperation(value = "Lấy thông tin user theo id", response = UserDto.class)
+//    Neu tra ve message khong thi la String.class
+    @ApiResponses({
+            @ApiResponse(code=404, message = "Không tìm thấy user"),
+            @ApiResponse(code=500, message = "")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<?> getUserById(@PathVariable int id) {
         UserDto result = userService.getUserById(id);
+//        if (result == null) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("no result");
+//        }
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
+    @ApiOperation(value = "Tạo user", response = UserDto.class)
+    @ApiResponses({
+            @ApiResponse(code=500, message = ""),
+            @ApiResponse(code=400, message = "Email đã tồn tại trong hệ thống")
+    })
     @PostMapping("")
-    public ResponseEntity<?> register(@RequestBody User user) {
-        UserDto result = userService.register(user);
+    public ResponseEntity<?> createUser(@Valid @RequestBody CreateUserReq req) {
+        UserDto result = userService.createUser(req);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
