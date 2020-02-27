b766761f20e9e736a0be6dd351fc3bc6243d0423
anhtunanh <linhadamk61vnuhus@gmail.com>
2020-02-28 06:58:51 +0700

    update thymeleaf templates

--- src/main/java/com/example/usermanagementrestapi/controller/ThymeleafController.java
+++ src/main/java/com/example/usermanagementrestapi/controller/ThymeleafController.java
@@ -1,8 +1,5 @@
 package com.example.usermanagementrestapi.controller;
 
-import com.example.usermanagementrestapi.entity.Product;
-import io.swagger.models.Model;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -29,6 +26,11 @@ public class ThymeleafController {
     @GetMapping("/product-details")
     public String productDetails() {
         return "product-details";
+    }
+
+    @GetMapping("/wishlist")
+    public String wishList() {
+        return "wishlist";
     }
 
     @GetMapping("/shop")
