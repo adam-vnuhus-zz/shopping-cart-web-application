316588b082cbc4acc6eff98754a4f85d8611e51e
anhtunanh <linhadamk61vnuhus@gmail.com>
2020-03-24 23:50:51 +0700

    frontend uppp

---
+++ src/main/resources/static/js/client/js/home.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    $(".add-to-cart").on("click", function () {
+        var dataCart = {};
+        var product = $(this).attr("prodid");
+        dataCart.amount = 1;
+        dataCart.productId = product;
+        // dataCart.guid = getCookie("guid");
+
+        NProgress.start();
+        var linkPost = "/api/carts";
+
+        axios.post(linkPost, dataCart).then(function(res){
+            NProgress.done();
+            if(res.data.success) {
+                swal(
+                    'Success',
+                    res.data.message,
+                    'success'
+                ).then(function() {
+                    location.replace("/cart/add");
+                });
+            } else {
+                swal(
+                    'Bạn cần đăng nhập!',
+                    res.data.message,
+                    'error'
+                ).then(function() {
+                    location.replace("/login");
+                });
+            }
+        }, function(err){
+            NProgress.done();
+            swal(
+                'Error',
+                'Fail',
+                'error'
+            );
+        });
+    });
+
+});
